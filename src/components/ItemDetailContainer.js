fb73fbf3c4aa173a9c0ad390cfca6595b9c9cca4
sofia <poncesofia@hotmail.com>
2021-08-26 23:47:05 -0300

    entrega proyecto final

---
+++ src/components/ItemDetailContainer.js
@@ -0,0 +1,50 @@
+{/*import React, {useEffect, useState}from 'react'
+import {ItemDetail} from './itemDetail'
+
+const items = [
+    {
+        id:1,
+        title: 'Estados Unidos',
+        image: usa,
+        descripcion: 'Viajá al país más visitado del mundo! ',
+        precio: '150',
+
+    },
+
+    {
+        id:2,
+        title: 'Cancun',
+        image: mx,
+        descripcion: 'Relajate en una de las playas más lindas y exoticas del mundo ;)',
+        precio: '120',
+
+    },
+
+    {
+        id:3,
+        title: 'Punta Cana',
+        image: pc, 
+        descripcion: 'Necesitas relajarte? Sin dudas, el mejor lugar para lo que necesitas! ',
+        precio: '90', 
+    }
+]
+
+function itemDetailContainer(){
+
+    const [datosDelItem, setDatosDelItem] = useState ({});
+
+    useEffect (()=>{
+        getItems()
+        .then((items)=>
+        setDatosDelItem(items)
+    })
+
+
+, [])
+
+return <ItemDetail item={datosDelItem}/>
+}
+
+
+export default itemDetailContainer */}
+
---
+++ src/components/itemDetail.js
@@ -0,0 +1,12 @@
+{/*import React from 'react'
+
+
+function ItemDetail({items}){
+
+
+    return<>
+    <h4>{items.title}</h4>
+    </>
+}
+
+export default ItemDetail*/}
