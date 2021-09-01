493798c2454a6acee058501e2131629b11b6ad32
sofia <poncesofia@hotmail.com>
2021-08-31 23:47:23 -0300

    terminar compra

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
 import 'semantic-ui-css/semantic.min.css'
-import Itemcount from './ItemCount';
+
 
 
 function Item({title, imagenSrc, descripcion, precio}) {
@@ -13,7 +13,6 @@ function Item({title, imagenSrc, descripcion, precio}) {
                 <p className="card-text text-secondary"> {descripcion}</p>
                 <p className="card-text text-secondary"> Precio desde: ${precio}</p>
                 <a href="#!" className="btn btn-outline-secondary rounded-0">Ver más</a>
-                <Itemcount/>
             </div>
             
         </div>
--- src/components/itemDetail.js
+++ src/components/itemDetail.js
@@ -1,12 +1,23 @@
-{/*import React from 'react'
+import React, {useState} from 'react';
+import React from 'react';
+import Itemcount from './ItemCount';
+import { Button } from 'semantic-ui-react';
+
+
+
 
 
 function ItemDetail({items}){
 
+    
+    
+ 
 
     return<>
     <h4>{items.title}</h4>
+    <Itemcount/>
+    <button>Terminar compra</button>
     </>
 }
 
-export default ItemDetail*/}
+export default ItemDetail
